8e2e6372494c08e974787ca3404b8d57f522b8e7
Justin Hollmer <justin.hollmer@gmail.com>
2022-09-28 14:32:40 -0500

    Finally starting to get somewhere

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import logo from '../images/logo.svg';
 import './App.css';
+import './Navbar.js';
+import Navbar from "./Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import "./Navbar.css";
+
+
+function Navbar() {
+    return (
+        <div>
+            <nav>
+                <h1>This is the H1</h1>
+                <ul>
+                    <li>Test1</li>
+                    <li>Test2</li>
+                    <li>Test3</li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar;
