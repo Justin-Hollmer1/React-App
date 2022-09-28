212076006a239a7677e65cd0edbd1d9ab157345a
Justin Hollmer <justin.hollmer@gmail.com>
2022-09-28 14:23:04 -0500

    did some stuff

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import logo from './images/logo.svg';
+import logo from '../images/logo.svg';
 import './App.css';
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is where I put things
         </p>
         <a
           className="App-link"
